f6976e0d8e8ee6743948f3f292fcbe9dd5c5f453
MicaFurno <miqui.furno@gmail.com>
2021-06-06 18:59:28 -0300

    primer commit

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+/*!
+* Start Bootstrap - Business Casual v6.0.0 (https://startbootstrap.com/theme/business-casual)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-casual/blob/master/LICENSE)
+*/
+// Highlights current date on contact page
+function iniciarMap(){
+    var coord = {lat:-34.5956145 ,lng: -58.4431949};
+    var map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 10,
+      center: coord
+    });
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
+}
